3428f316e8004bee7136e849ea8be8cf626cb920
ja963mes <james.barrera@stonybrook.edu>
2025-09-16 14:31:36 -0400

    Add files via upload

--- PlusOneArray.java
+++ PlusOneArray.java
@@ -1,14 +1,16 @@
 public class PlusOneArray {
     public static int[] upArray(final int[] arr) {
-        int arrLen = arr.length;
-        String out = String.join(",", arr);
-        int outInt = Integer.parseInt(out);
-        outInt = outInt + 1;
-        out = Integer.toString(outInt);
-        int[] outArr = new int[out.length()];
-        for(int i = 0; i < out.length(); i++) {
-            outArr[i] = Character.getNumericValue(out.charAt(i));
+        if (arr == null || arr.length == 0) return null;
+        for (int d : arr) if (d < 0 || d > 9) return null;
+
+        int[] res = arr.clone();
+        for (int i = res.length - 1; i >= 0; i--) {
+            if (res[i] < 9) { res[i]++; return res; }
+            res[i] = 0;
         }
-        return outArr;
+
+        int[] expanded = new int[res.length + 1];
+        expanded[0] = 1;
+        return expanded;
     }
 }
